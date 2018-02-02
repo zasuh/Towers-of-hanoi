d1b29474a173562e14ff6e2082bf9a5c399b17f1
Zane Suhadolnik <zanesuhi@gmail.com>
2018-02-02 17:44:13 +0100

    Updated

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //A function that takes away 5 blocks so 5 are left (player clicks 5 button)
-var btn5 = document.getElementById('btn-5');
+const btn5 = document.getElementById('btn-5');
 btn5.addEventListener('click', updateBlocks);
 
 function updateBlocks() {
-    
+
 }
+
 //A function that takes away 4 blocks so 6 are left (player clicks 6 button)
 
 //A function that takes away 3 blocks so 7 are left (player clicks 7 button)
