10d4790d4314cb48d8c9114c10c5727b09a1c556
Zane Suhadolnik <zanesuhi@gmail.com>
2018-02-02 06:43:40 +0100

    Attempt to add event listener to buttons

---
+++ script.js
@@ -0,0 +1,18 @@
+//A function that takes away 5 blocks so 5 are left (player clicks 5 button)
+var btn5 = document.getElementById('btn-5');
+btn5.addEventListener('click', updateBlocks);
+
+function updateBlocks() {
+    
+}
+//A function that takes away 4 blocks so 6 are left (player clicks 6 button)
+
+//A function that takes away 3 blocks so 7 are left (player clicks 7 button)
+
+//A function that takes away 2 blocks so 8 are left (player clicks 8 button)
+
+//A function that takes away 1 block so 9 are left (player clicks 9 button)
+
+//A function that puts all available blocks on screen (player hits 10 button)
+
+//Function that starts the solver
